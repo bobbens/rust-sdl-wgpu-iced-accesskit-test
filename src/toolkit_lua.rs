a8f6212c9fa092ec98e1d993a41e276537dff41e
Koranir <101683475+Koranir@users.noreply.github.com>
2024-11-03 19:40:02 +1100

    aaaaah

--- src/toolkit_lua.rs
+++ src/toolkit_lua.rs
@@ -3,9 +3,10 @@ use iced_core::{Element, Theme};
 use iced_runtime::{Program, Task};
 use iced_wgpu::Renderer;
 use iced_widget::{button, column, container};
+use mlua::{Chunk, FromLuaMulti, UserData};
 
 #[derive(Debug, Clone)]
-struct Message(mlua::Value);
+pub struct Message(mlua::Value);
 impl Message {
     fn new(value: mlua::Value) -> Message {
         Message(value)
@@ -14,6 +15,9 @@ impl Message {
 impl mlua::UserData for Message {}
 
 struct LuaElement(iced::Element<'static, Message, Theme, Renderer>);
+/// Safety:
+/// Not safe at _all_. Try to ensure that the base element is `Send`
+unsafe impl Send for LuaElement {}
 impl Into<iced::Element<'static, Message, Theme, Renderer>> for LuaElement {
     fn into(self) -> iced::Element<'static, Message, Theme, Renderer> {
         self.0
@@ -21,18 +25,39 @@ impl Into<iced::Element<'static, Message, Theme, Renderer>> for LuaElement {
 }
 impl mlua::UserData for LuaElement {}
 
-struct LuaContainer(container::Container<'static, Message, Theme, Renderer>);
+struct LuaContainer(Option<container::Container<'static, Message, Theme, Renderer>>);
+unsafe impl Send for LuaContainer {}
 impl LuaContainer {
-    fn new(content: impl Into<Element<'static, Message, Theme, Renderer>>) -> LuaContainer {
-        LuaContainer(container(content))
+    fn new<C: Into<Element<'static, Message, Theme, Renderer>> + Send>(content: C) -> LuaContainer {
+        LuaContainer(Some(container(content)))
     }
 }
 impl mlua::UserData for LuaContainer {
     fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
-        // Constructor
-        methods.add_meta_function(mlua::MetaMethod::Call, |_, context: LuaElement| {
-            Ok(LuaContainer::new(context))
+        methods.add_method_mut("padding", |_lua, this, padding: f32| {
+            dbg!();
+            let inner = std::mem::replace(this, LuaContainer(None));
+            let Some(container) = inner.0 else {
+                return Err(mlua::Error::UserDataDestructed);
+            };
+            Ok(LuaContainer(Some(container.padding(padding))))
         });
+    }
+}
+
+impl mlua::FromLuaMulti for LuaElement {
+    fn from_lua_multi(values: mlua::MultiValue, lua: &mlua::Lua) -> mlua::Result<Self> {
+        Ok(match values.len() {
+            0 => LuaElement(iced_widget::Space::with_width(0).into()),
+            1 => match &values[0] {
+                mlua::Value::UserData(any_user_data) => any_user_data.take::<Self>()?,
+                mlua::Value::String(s) => {
+                    LuaElement(iced_widget::text(s.to_str().unwrap().to_string()).into())
+                }
+                _ => todo!(),
+            },
+            _ => todo!(),
+        })
     }
 }
 
@@ -40,6 +65,7 @@ macro_rules! impl_fromlua_for {
   ($($typename:ty),*) => {$(
     impl mlua::FromLua for $typename {
       fn from_lua(value: mlua::Value, _lua: &mlua::Lua) -> mlua::Result<Self> {
+          dbg!(&value);
         match value {
           mlua::Value::UserData(ud) => Ok(ud.take::<Self>()?),
           _ => unreachable!()
@@ -48,7 +74,7 @@ macro_rules! impl_fromlua_for {
    }
  )*}
 }
-impl_fromlua_for!(LuaElement, LuaContainer, Message);
+impl_fromlua_for!(LuaContainer, Message);
 
 #[derive(Debug)]
 pub struct ToolkitLua {
@@ -60,6 +86,28 @@ pub struct ToolkitLua {
 impl ToolkitLua {
     pub fn new() -> ToolkitLua {
         let lua = mlua::Lua::new();
+
+        let element = lua
+            .create_function(|lua, widget: mlua::MultiValue| {
+                LuaElement::from_lua_multi(widget, lua)
+            })
+            .unwrap();
+        lua.globals().set("Element", element).unwrap();
+        let container = lua
+            .create_function(|_, element: LuaElement| Ok(LuaContainer::new(element)))
+            .unwrap();
+        lua.globals().set("Container", container).unwrap();
+
+        lua.load("function update() end").exec().unwrap();
+        lua.load(
+            "function view() 
+                local element = Element(\"wtf\")
+                local container = Container(Element(\"Hi world\")):padding(2.0)
+            end",
+        )
+        .exec()
+        .unwrap();
+
         let globals = lua.globals();
         ToolkitLua {
             lua,
